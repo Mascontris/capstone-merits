233f798b962237ebe9410724b8d630777b70a11a
Igor <Mascontris@yahoo.com>
2019-12-27 21:41:50 -0700

    created households post route

--- src/routers/household_router.js
+++ src/routers/household_router.js
@@ -23,49 +23,34 @@ householdRouter
       })
       .catch(next)
   })
-//   .post(bodyParser, (req, res, next) => {
-//     for (const field of ['title', 'url', 'rating']) {
-//       if (!req.body[field]) {
-//         logger.error(`${field} is required`)
-//         return res.status(400).send({
-//           error: { message: `'${field}' is required` }
-//         })
-//       }
-//     }
+  .post(bodyParser, (req, res, next) => {
+    for (const field of ['name', 'password']) {
+      if (!req.body[field]) {
+        logger.error(`${field} is required`)
+        return res.status(400).send({
+          error: { message: `'${field}' is required` }
+        })
+      }
+    }
 
-//     const { title, url, description, rating } = req.body
+     const { name, password } = req.body
 
-//     const ratingNum = Number(rating)
+     const newHousehold = { name, password }
 
-//     if (!Number.isInteger(ratingNum) || ratingNum < 0 || ratingNum > 5) {
-//       logger.error(`Invalid rating '${rating}' supplied`)
-//       return res.status(400).send({
-//         error: { message: `'rating' must be a number between 0 and 5` }
-//       })
-//     }
-
-//     if (!isWebUri(url)) {
-//       logger.error(`Invalid url '${url}' supplied`)
-//       return res.status(400).send({
-//         error: { message: `'url' must be a valid URL` }
-//       })
-//     }
-
-//     const newBookmark = { title, url, description, rating }
-
-//     BookmarksService.insertBookmark(
-//       req.app.get('db'),
-//       newBookmark
-//     )
-//       .then(bookmark => {
-//         logger.info(`Bookmark with id ${bookmark.id} created.`)
-//         res
-//           .status(201)
-//           .location(`/bookmarks/${bookmark.id}`)
-//           .json(serializeBookmark(bookmark))
-//       })
-//       .catch(next)
-//   })
+    HouseholdsService.insertHousehold(
+      req.app.get('db'),
+      newHousehold
+    )
+      .then(household => {
+        logger.info(`household with id ${household.id} created.`)
+        res
+          .status(201)
+          .location(`/households`)
+          //.location(`/households/${household.id}`)
+          .json(serializeHousehold(household))
+      })
+      .catch(next)
+  })
 
 // bookmarksRouter
 //   .route('/bookmarks/:bookmark_id')
